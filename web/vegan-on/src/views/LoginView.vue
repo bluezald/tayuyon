1248194bb63c6da02c8d2f430aa4e7a90b9fd7eb
Vincent Bacalso <bacalso.vincent@gmail.com>
2020-05-17 19:58:20 +0800

    Updated login view

--- web/vegan-on/src/views/LoginView.vue
+++ web/vegan-on/src/views/LoginView.vue
@@ -1,53 +1,63 @@
 <template>
   <div>
-    <section class="hero is-success is-fullheight">
-      <div class="hero-body">
-        <div class="container has-text-centered">
-          <div class="column is-4 is-offset-4">
-            <h3 class="title has-text-black">Login</h3>
-            <hr class="login-hr">
-            <p class="subtitle has-text-black">Please login to proceed.</p>
-            <div class="box">
-              <figure class="avatar">
-                <img src="https://placehold.it/128x128">
-              </figure>
-              <form>
-                <div class="field">
-                  <div class="control">
-                    <input class="input is-large" type="email" placeholder="Your Email" autofocus="">
-                  </div>
-                </div>
+    <section class="section card">
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column"><img src="https://bulma.dev/placeholder/pictures/bg_16-9.svg?primary=00d1b2" alt=""></div>
+            <div class="column">
+              <div class="hero-body">
+                <div class="container has-text-centered">
+                  <div class="column is-4 is-offset-4">
+                    <div>
+                      <figure class="avatar">
+                        <img :src="logo">
+                      </figure>
+                      <form>
+                        <div class="field">
+                          <div class="control">
+                            <input class="input" type="email" placeholder="Your Email" autofocus="">
+                          </div>
+                        </div>
 
-                <div class="field">
-                  <div class="control">
-                    <input class="input is-large" type="password" placeholder="Your Password">
+                        <div class="field">
+                          <div class="control">
+                            <input class="input" type="password" placeholder="Your Password">
+                          </div>
+                        </div>
+                        <div class="field">
+                          <label class="checkbox">
+                            <input type="checkbox">
+                            Remember me
+                          </label>
+                        </div>
+                        <button class="button is-block is-success is-fullwidth">Login <i class="fa fa-sign-in"
+                            aria-hidden="true"></i></button>
+                      </form>
+                    </div>
+                    <p class="has-text-grey">
+                      <a href="../">Sign Up</a> &nbsp;·&nbsp;
+                      <a href="../">Forgot Password</a> &nbsp;·&nbsp;
+                      <a href="../">Need Help?</a>
+                    </p>
                   </div>
                 </div>
-                <div class="field">
-                  <label class="checkbox">
-                    <input type="checkbox">
-                    Remember me
-                  </label>
-                </div>
-                <button class="button is-block is-info is-large is-fullwidth">Login <i class="fa fa-sign-in"
-                    aria-hidden="true"></i></button>
-              </form>
+              </div>
             </div>
-            <p class="has-text-grey">
-              <a href="../">Sign Up</a> &nbsp;·&nbsp;
-              <a href="../">Forgot Password</a> &nbsp;·&nbsp;
-              <a href="../">Need Help?</a>
-            </p>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   </div>
 </template>
 
 <script>
+import logo from '@/assets/logo-sm.png'
 export default {
   name: 'LoginView',
-  components: {}
+  components: {},
+  data() {
+    return {
+      logo: logo
+    }
+  }
 }
 </script>
